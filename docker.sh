7218a8242ab967d143a70766c0f06b611c6943aa
xxxxx@163.com <xxxxx@163.com>
2023-02-11 22:04:04 +0800

    升级安装脚本，限制docker的log最大100M；

--- docker.sh
+++ docker.sh
@@ -25,7 +25,7 @@ if [[ -d "$(pwd)"/auto_spy_data/autospy  ]]; then
 	docker stop auto_spy
 	docker rm auto_spy
 	echo "autospy配置文件已存在"
-	docker run -dit --restart=always --name=auto_spy  -v "$(pwd)"/auto_spy_data/autospy:/autospy --hostname=auto_spy xieshang1111/auto_spy:$arch
+	docker run -dit --restart=always --name=auto_spy --log-opt max-size=100m --log-opt max-file=3 -v "$(pwd)"/auto_spy_data/autospy:/autospy --hostname=auto_spy xieshang1111/auto_spy:$arch
 else
 	mkdir auto_spy_data
     docker run -dit --restart=always --name=auto_spy --hostname=auto_spy xieshang1111/auto_spy:$arch
@@ -35,7 +35,7 @@ else
 	echo -n -e "检测到autospy配置文件不存在，如之前直装过auto_spy,请输入绝对路径以完成配置文件一键迁移(如没安装过，直接回车即可)："
 	read dir
 	if [[ $dir != "" ]];then
-	docker run -dit --restart=always --name=auto_spy  -v "$(pwd)"/auto_spy_data/autospy:/autospy --hostname=auto_spy xieshang1111/auto_spy:$arch
+	docker run -dit --restart=always --name=auto_spy --log-opt max-size=100m --log-opt max-file=3 -v "$(pwd)"/auto_spy_data/autospy:/autospy --hostname=auto_spy xieshang1111/auto_spy:$arch
 	cp $dir/auto_spy.yaml "$(pwd)"/auto_spy_data/autospy
 	docker exec -it auto_spy bash /autospy/start.sh
 	exit 0
@@ -44,7 +44,7 @@ else
 	docker cp auto_spy:/autospy/ "$(pwd)"/auto_spy_data/autospy
 	docker stop auto_spy
 	docker rm auto_spy
-	docker run -dit --restart=always --name=auto_spy  -v "$(pwd)"/auto_spy_data/autospy:/autospy --hostname=auto_spy xieshang1111/auto_spy:$arch
+	docker run -dit --restart=always --name=auto_spy --log-opt max-size=100m --log-opt max-file=3 -v "$(pwd)"/auto_spy_data/autospy:/autospy --hostname=auto_spy xieshang1111/auto_spy:$arch
 	cp "$(pwd)"/auto_spy_data/autospy/auto_spy_simple.yaml "$(pwd)"/auto_spy_data/autospy/auto_spy.yaml
 	echo -e "现在可以去目录下[auto_spy_data/auto_spy]里的[auto_spy.yaml]修改信息了\n修改完成后，请再次运行脚本\n"
 	exit 0
